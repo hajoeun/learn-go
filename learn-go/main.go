6b84bbe01bc2ef1d69c3c45e8dfa933c1a32aa5e
joeunha <imjoeunha@gmail.com>
2020-02-15 16:44:26 +0900

    Learn if and multi-return

--- learn-go/main.go
+++ learn-go/main.go
@@ -2,48 +2,27 @@ package main
 
 import "fmt"
 
-func sum1(a int, b int, c int) int {
-	return a + b + c
+func isEven(n int) (bool, int) {
+	if v := n % 2; v == 0 {
+		return true, v
+	} else {
+		return false, v
+	}
 }
 
-func sum2(numbers [3]int) (total int) {
-	number := 0
-	for i := 0; i < len(numbers); i++ {
-		number = numbers[i]
-		total += number
+func isOdd(n int) (bool, int) {
+	v := n % 2
+	if v == 0 {
+		return false, v
 	}
-	return total
-}
-
-func sum3(numbers ...int) (total int) {
-	number := 0
-	for i := 0; i < len(numbers); i++ {
-		number = numbers[i]
-		total += number
-	}
-	return total
-}
-
-func sum4(numbers ...int) (total int) {
-	for i := range numbers {
-		total += numbers[i]
-	}
-	return total
-}
-
-func sum5(numbers ...int) (total int) {
-	for _, number := range numbers {
-		total += number
-	}
-	return total
+	return true, v
 }
 
 func main() {
-	fmt.Println(
-		sum1(1, 2, 3),
-		sum2([3]int{1, 2, 3}),
-		sum3(1, 2, 3, 4, 5),
-		sum4(1, 2, 3, 4, 5),
-		sum5(1, 2, 3, 4, 5),
-	)
+	number := 10
+	isEvenNumber, evenResult := isEven(number)
+	isOddNumber, oddResult := isOdd(number)
+
+	fmt.Println(isEvenNumber, evenResult)
+	fmt.Println(isOddNumber, oddResult)
 }
