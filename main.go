fbde3fe3945a319c45d661d7aaab8d978247e580
Joeun <imjoeunha@gmail.com>
2020-08-25 00:23:56 +0900

    Create first goroutine

--- main.go
+++ main.go
@@ -1,44 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-	urls := []string{
-		"https://www.naver.com",
-		"https://www.youtube.com",
-		"https://www.banksalad.com",
-		"https://www.facebook.com",
-		"https://www.github.com",
-		"https://www.google.com",
-		"https://www.brunch.co.kr",
-	}
-	results := make(map[string]string)
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go count("foo", 10)
+	go count("bar", 5)
+	count("uuu", 5)
 }
 
-var errRequestFailed = errors.New("Request Failed")
-
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+func count(name string, len int) {
+	for i := 0; i < len; i++ {
+		fmt.Println(name, "count", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
