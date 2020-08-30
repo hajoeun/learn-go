b1f27757cf69053d2b8efbc3edb6ea62d3b20485
Joeun <imjoeunha@gmail.com>
2020-08-30 16:57:02 +0900

    Move channel code to syntax package

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"github.com/joeunha/learn-go/syntax"
 )
 
 func main() {
-	channel := make(chan string)
-	people := [5]string{"foo", "bar", "vvv", "kkk", "sss"}
-
-	for _, person := range people {
-		go isPerson(person, channel)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println("wating for ", i)
-		fmt.Println(<-channel) // blocking operation
-	}
-}
-
-func isPerson(person string, channel chan string) {
-	time.Sleep(time.Second * 2)
-	channel <- person + "'s send from channel"
+	syntax.Channel()
 }
---
+++ syntax/channel.go
@@ -0,0 +1,25 @@
+package syntax
+
+import (
+	"fmt"
+	"time"
+)
+
+// Channel is example for channel syntax
+func Channel() {
+	channel := make(chan string)
+	people := [5]string{"foo", "bar", "vvv", "kkk", "sss"}
+
+	for _, person := range people {
+		go isPerson(person, channel)
+	}
+	for i := 0; i < len(people); i++ {
+		fmt.Println("wating for ", i)
+		fmt.Println(<-channel) // blocking operation
+	}
+}
+
+func isPerson(person string, channel chan string) {
+	time.Sleep(time.Second * 2)
+	channel <- person + "'s send from channel"
+}
