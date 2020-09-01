c221e8eb69405c326f822c7a342f456062e84214
Joeun <imjoeunha@gmail.com>
2020-09-02 01:41:38 +0900

    Update getPage

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type jobCardData struct {
+	id       string
+	title    string
+	company  string
+	location string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=javascript&limit=50"
 
 func main() {
@@ -21,7 +29,32 @@ func main() {
 
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println(pageURL)
+
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkStatusCode(res.StatusCode)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	jobCards := doc.Find(".jobsearch-SerpJobCard")
+
+	jobCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title, _ := card.Find("a.jobtitle").Attr("title")
+		company := card.Find(".sjcl>.company").Text()
+		location := card.Find(".sjcl>.location").Text()
+		summary := card.Find(".summary").Text()
+		jobData := jobCardData{
+			id:       id,
+			title:    title,
+			company:  company,
+			location: location,
+			summary:  summary,
+		}
+
+		fmt.Println(jobData)
+	})
 }
 
 func getTotalPages() int {
