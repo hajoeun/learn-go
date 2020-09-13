ec312628ed4329b05a2af27f42db086cbf5e0700
joeunha <imjoeunha@gmail.com>
2020-09-13 15:00:10 +0900

    Update formatting verb

--- syntax/formattingverb.go
+++ syntax/formattingverb.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // FormattingVerb should print formatting verbs (형식 동사)
 func FormattingVerb() {
+	fmt.Println("===== 형식 동사 기본 =====")
 	fmt.Printf("[%#v] A float: %f\n", "%f", 3.141592)
 	fmt.Printf("[%#v] An integer: %d\n", "%d", 15)
 	fmt.Printf("[%#v] A string: %s\n", "%s", "hello")
@@ -12,4 +13,11 @@ func FormattingVerb() {
 	fmt.Printf("[%#v] Values: %#v %#v %#v\n", "%#v", 1.2, "\t", true)
 	fmt.Printf("[%#v] Types: %T %T %T\n", "%T", 1.2, "\t", true)
 	fmt.Printf("[%#v] Percent sign: %%\n", "%%")
+
+	fmt.Println("===== 최소 너비 지정 =====")
+	fmt.Printf("%12s | %s\n", "Product", "Cost in cents")
+	fmt.Println("--------------------------")
+	fmt.Printf("%12s | %2d\n", "Stamps", 50)
+	fmt.Printf("%12s | %2d\n", "Paper Clips", 5)
+	fmt.Printf("%12s | %2d\n", "Tape", 99)
 }
